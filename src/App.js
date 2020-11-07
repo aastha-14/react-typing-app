eec1f423c371d1c16fd31227be6f4a324c6792ca
Aastha Gupta <aastha1406@gmail.com>
2020-11-07 23:51:24 +0530

    Timer Added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { words } from './utils/words';
-import { currentTime } from './utils/time';
-import useKeyPress from './hooks/useKeyPress';
-
-const initialWords = words();
+import './bootstrap.min.css';
+import { useState } from 'react';
+import Typing from './components/Typing';
 
 function App() {
-  const [leftPadding, setLeftPadding] = useState(new Array(20).fill(' ').join(''));
-  const [outgoingChars, setOutgoingChars] = useState('');
-  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
-  const [incomingChars, setIncomingChars] = useState(initialWords.substr(1));
-
-  const [startTime, setStartTime] = useState();
-  const [wordCount, setWordCount] = useState(0);
-  const [wpm, setWpm] = useState(0);
-
-  useKeyPress(key => {
-    let updatedOutgoingChars = outgoingChars;
-    let updatedIncomingChars = incomingChars;
-    if (!startTime) {
-      setStartTime(currentTime());
-    }
-    if (key === currentChar) {
-      if (leftPadding.length > 0) {
-        setLeftPadding(leftPadding.substring(1));
-      }
-      updatedOutgoingChars += currentChar;
-      setOutgoingChars(updatedOutgoingChars);
-      setCurrentChar(incomingChars.charAt(0));
-      updatedIncomingChars = incomingChars.substring(1);
-      if (updatedIncomingChars.split(' ').length < 10) {
-        updatedIncomingChars += ' ' + words();
-      }
-      setIncomingChars(updatedIncomingChars);
-      if (incomingChars.charAt(0) === ' ') {
-        setWordCount(wordCount + 1);
-        const durationInMinutes = (currentTime() - startTime) / 6000.0;
-        setWpm(((wordCount + 1) / durationInMinutes).toFixed(2));
-      }
-    }
-  });
+  const [typing, setTyping] = useState(false);
   return (
     <div className="App">
+      <h1>Typing Insanity</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="Character">
-          <span className="Character-out">
-            {(leftPadding + outgoingChars).slice(-20)}
-          </span>
-          <span className="Character-current">{currentChar}</span>
-          <span>{incomingChars.substr(0, 20)}</span>
-        </p>
-        <h3>WPM: {wpm}</h3>
+        {typing
+          ? <><Typing /></>
+          : <button className='btn btn-danger disabled btn-lg' onClick={
+            () => setTyping(true)
+          }>Start</button>}
       </header>
     </div>
   );
---
+++ src/components/Typing.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { currentTime } from '../utils/time';
+import useKeyPress from '../hooks/useKeyPress';
+import { words } from '../utils/words';
+const initialWords = words();
+
+function Typing() {
+    const [leftPadding, setLeftPadding] = useState(new Array(20).fill(' ').join(''));
+    const [outgoingChars, setOutgoingChars] = useState('');
+    const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
+    const [incomingChars, setIncomingChars] = useState(initialWords.substr(1));
+
+    const [startTime, setStartTime] = useState();
+    const [wordCount, setWordCount] = useState(0);
+    const [wpm, setWpm] = useState(0);
+
+    const [accuracy, setAccuracy] = useState(0);
+    const [typedChars, setTypedChars] = useState('');
+
+    useKeyPress(key => {
+        let updatedOutgoingChars = outgoingChars;
+        let updatedIncomingChars = incomingChars;
+        if (!startTime) {
+            setStartTime(currentTime());
+        }
+        if (key === currentChar) {
+            if (leftPadding.length > 0) {
+                setLeftPadding(leftPadding.substring(1));
+            }
+            updatedOutgoingChars += currentChar;
+            setOutgoingChars(updatedOutgoingChars);
+            setCurrentChar(incomingChars.charAt(0));
+            updatedIncomingChars = incomingChars.substring(1);
+            if (updatedIncomingChars.split(' ').length < 10) {
+                updatedIncomingChars += ' ' + words();
+            }
+            setIncomingChars(updatedIncomingChars);
+            if (incomingChars.charAt(0) === ' ') {
+                setWordCount(wordCount + 1);
+                const durationInMinutes = (currentTime() - startTime) / 6000.0;
+                setWpm(((wordCount + 1) / durationInMinutes).toFixed(2));
+            }
+        }
+        const updatedTypedChars = typedChars + key;
+        setTypedChars(updatedTypedChars);
+        setAccuracy(((updatedOutgoingChars.length * 100) / updatedTypedChars.length).toFixed(2));
+    });
+
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds => seconds - 1);
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval();
+                } else {
+                    setMinutes(minutes => minutes - 1);
+                    setSeconds(59);
+                }
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [seconds, minutes]);
+    return (
+        <div>
+            {minutes === 0 && seconds === 0 ?
+                <>
+                    <h2>
+                        Time's Up!
+                    </h2>
+                    <div class="alert alert-dismissible alert-info">
+                        <h3>WPM: {wpm} | Accuracy: {accuracy}%</h3>
+                    </div>
+                </>
+                : <>
+                    <h3>Time Remaining  {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h3>
+                    <p className="Character">
+                        <span className="Character-out">
+                            {(leftPadding + outgoingChars).slice(-20)}
+                        </span>
+                        <span className="btn-danger disabled">{currentChar}</span>
+                        <span>{incomingChars.substr(0, 20)}</span>
+                    </p>
+                    <h2>WPM: {wpm} | Accuracy: {accuracy}%</h2>
+                </>
+            }
+
+        </div>
+    );
+}
+
+export default Typing;
